5e0e43c75d9b6ee24c85fede57f32a0f33085d3e
Bas <bas@heppen.be>
2025-06-26 13:53:42 +0200

    Add files via upload

---
+++ ExtIO_sddc/tdialog.h
@@ -0,0 +1,22 @@
+#ifndef _TABDIALOGH_
+#define _TABDIALOGH_
+
+#include "license.txt" 
+#include "framework.h"
+#include "resource.h"
+#include "config.h"
+#include <stdio.h>
+
+extern bool bSupportDynamicSRate;
+extern void UpdatePPM(HWND hWnd);
+
+INT_PTR CALLBACK DlgMainFn(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT_PTR CALLBACK DlgSelectDevice(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+struct DevContext 
+{
+	unsigned char numdev;
+	char dev[MAXNDEV][MAXDEVSTRLEN];
+};
+#endif // _TABDIALOGH_
